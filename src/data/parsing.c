b875866fcb077a70b602a5c1c12b3640d0c51993
Gael Monachon <gmonacho@student.le-101.fr>
2019-04-27 10:03:51 +0200

    merge parsing

--- src/data/parsing.c
+++ src/data/parsing.c
@@ -2,23 +2,23 @@
 #include "data.h"
 #include "libft.h"
 
-void    ft_read_map(t_sector *data, int fd)
-{   
-    char *line;
+// void    ft_read_map(t_sector *data, int fd)
+// {   
+//     char *line;
 
-    while (get_next_line(fd, line))
-}
+//     while (get_next_line(fd, line))
+// }
 
-int main (int argc, char **argv)
-{
-    int fd;
+// int main (int argc, char **argv)
+// {
+//     int fd;
 
-    if (argc = 2)
-    {
-        if (fd = open(argv[1], O_RDONLY) <= 0)
-            return (-1);
-    }
-    else
-        ft_putendl("pas d'argument");
-    return (0);
-}
+//     if (argc = 2)
+//     {
+//         if (fd = open(argv[1], O_RDONLY) <= 0)
+//             return (-1);
+//     }
+//     else
+//         ft_putendl("pas d'argument");
+//     return (0);
+// }
