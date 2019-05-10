637b4b6f64282ac8408e2ee3fbef7dce582fb0cd
Alexandre Ducimetiere <aducimet@z4r1p4.le-101.fr>
2019-05-10 17:41:08 +0200

    upload aducimet

--- src/editor_loop/editor_event.c
+++ src/editor_loop/editor_event.c
@@ -12,18 +12,18 @@ int			editor_event(t_win *win, t_map *map, SDL_bool *loop)
 	SDL_PollEvent(&event);
 	if (event.type == SDL_QUIT)
 		*loop = SDL_FALSE;
-	else if(event.type == SDL_MOUSEWHEEL)
+	/*else if(event.type == SDL_MOUSEWHEEL)
 	{
 		if (event.wheel.y > 0)
-			map_zoom(map, 0.05);
+			//map_zoom(map, 0.05);
 		else if (event.wheel.y < 0)
 			map_zoom(map, -0.05);
-	}
-	if (win->mouse->button[MOUSE_LEFT].pressing)
+	}*/
+	/*if (win->mouse->button[MOUSE_LEFT].pressing)
 	{
 		if (!map_add_line(map, new_linedef((t_line){(t_dot){(win->mouse->x - map->x) / map->unit, (win->mouse->y - map->y) / map->unit}, (t_dot){(win->mouse->x - map->x) / map->unit, (win->mouse->y - map->y) / map->unit}}, NULL, NULL, 0)))
 			return (ret_perror("error map_add_line in editor_event"));
-	}
+	}*/
 	else if (win->mouse->button[MOUSE_LEFT].releasing)
 		map->lines[map->nb_lines - 1].p2 = (t_dot){(win->mouse->x - map->x) / map->unit, (win->mouse->y - map->y) / map->unit};
 	if (key_pressed(SDL_SCANCODE_W))
