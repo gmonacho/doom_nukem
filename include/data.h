d57ff883da686cb6b31a9cf80c3fc15109afe42e
Alexandre Ducimetiere <aducimet@z4r1p4.le-101.fr>
2019-05-10 17:39:27 +0200

    upload data.h

--- include/data.h
+++ include/data.h
@@ -16,7 +16,7 @@ typedef struct				s_linedef
 	SDL_Texture				*p1p2_texture;
 	SDL_Texture				*p2p1_texture;
 	Uint32					flags;
-	struct s_sector			*next;
+	struct s_linedef		*next;
 }							t_linedef;
 
 typedef struct				s_sector
@@ -51,9 +51,16 @@ typedef struct	s_map
 
 int			map_add_line(t_map *map, t_linedef line);
 int			map_get_nb_data(void **data);
-t_linedef	new_linedef(t_line line, SDL_Texture *p1p2, SDL_Texture *p2p1, Uint32 flags);
-void		map_zoom(t_win *win, t_map *map);
-int 		ft_data_storing(int fd, int fd1);
+//t_linedef	new_linedef(t_line line, SDL_Texture *p1p2, SDL_Texture *p2p1, Uint32 flags);
+//void		map_zoom(t_win *win, t_map *map);
+void 		ft_data_storing(int fd, int fd1);
+void		ft_find_coord_p1(t_sector *sector, char *tab);
+void		ft_find_coord_p2(t_sector *sector, char *tab);
+void		add_sector(t_sector **sectors, t_sector *new_sectors);
+void		add_linedef(t_linedef **linedef, t_linedef *new_linedef);
+t_sector	*new_sector(void);
+t_linedef	*new_linedef(void);
+int 		ft_parse_error(char **tab);
 
 
 #endif
