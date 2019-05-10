6a1787b6fb8fd2c18f3d72173ef2e45d60f9370a
Alexandre Ducimetiere <aducimet@z4r1p4.le-101.fr>
2019-05-10 17:28:51 +0200

    upload fichier

--- src/data/parsing.c
+++ src/data/parsing.c
@@ -4,108 +4,102 @@
 #include "data.h"
 #include "ret_error.h"
 
-void        add_sectors(t_sector **sectors, t_sector *new_sectors)
+void		ft_fill_coord(t_sector *sector, char **tab, int i)
 {
-    new_sectors->next = *sectors;
-    *sectors = new_sectors;
+	int y;
+	int flag;
+
+	y = 0;
+	flag = 0;
+	while (!(ft_strchr(tab[i], '}')))
+	{
+		if (ft_strstr(tab[i], "dot") && flag == 0)
+		{
+			ft_find_coord_p1(sector, tab[i]);
+			flag++;
+		}
+		else if (ft_strstr(tab[i], "dot") && flag != 0)
+			ft_find_coord_p2(sector, tab[i]);
+		i++;
+	}
 }
 
-t_sector    *new_sector()
-{   
-    t_sector *sector;
-
-    if (!(sector = (t_sector *)ft_memalloc(sizeof (t_sector))))
-        exit(0);
-    sector->floor_height = 0;
-    sector->ceil_height = 0;
-    sector->light_level = 0;
-    sector->floor_texture = NULL;
-    sector->ceil_texture = NULL;
-    sector->lines = NULL;
-    sector->next = NULL;
-    return (sector);
-}
-
-int     count_line(int fd1)
-{   
-    int nb;
-    char *line;
-
-    nb = 0;
-    while (get_next_line(fd1, &line) > 0)
-    {   
-        free(line);
-        nb++;
-    }
-    return (nb);
-}
-char    **ft_fill_map(int fd, int fd1)
+int			count_line(int fd1)
 {
-    char *line;
-    char **tab;
-    int i;
-    int nb;
+	int		nb;
+	char	*line;
 
-    i = 0;
-    nb = count_line(fd1);
-    if (!(tab = (char **)malloc(sizeof(char *) * (nb + 1))))
-        return (NULL);
-    while (get_next_line(fd, &line) > 0)
-    {
-        tab[i] = ft_strdup(line);
-        //printf("tab[%d] = %s\n",i ,tab[i]);
-        free(line);
-        printf("antoine la grosse salope\n");
-        i++;
-    }
-    tab[i] = NULL;
-    return (tab);
+	nb = 0;
+	while (get_next_line(fd1, &line) > 0)
+	{
+		free(line);
+		nb++;
+	}
+	return (nb);
 }
 
-int    ft_data_storing(int fd, int fd1)
+char		**ft_fill_map(int fd, int fd1)
 {
-    char **tab;
-    int i;
-    t_sector *sector;
-    int y;
-    int comptor;
-    char tmp[5];
+	char	*line;
+	char	**tab;
+	int		i;
+	int		nb;
 
-    i = 0;
-    y = 0;
-    comptor = 0;
-    sector = NULL;
-    tmp[4] = '\0';
-    tab = ft_fill_map(fd, fd1);
-    while (tab[i])
-    {   
-        if (ft_strstr(tab[i], "Sector"))
-        {
-            while ((ft_strchr(tab[i], '}') == NULL || ft_strchr(tab[i - 1], '}') == NULL))
-            {   
-                add_sectors(&sector, new_sector());
-                if (ft_strstr(tab[i], "floorHeight"))
-                    {   
-                        y = 0;
-                        comptor = 0;
-                        while (tab[i][y])
-                        {   
-                            if (ft_isdigit(tab[i][y]))
-                            {
-                                tmp[comptor] = tab[i][y];
-                                comptor++; 
-                            }
-                            //printf("tmp = %s\n",tmp);
-                            y++;
-                        }
-                        //printf("tmp 2= %s\n",tmp);
-                        sector->floor_height = ft_atoi(tmp);
-                        //printf("floor = %d\n", sector->floor_height);
-                    }
-                i++;
-            }
-        }
-        i++;
-    }
-    return (0);
+	i = 0;
+	nb = count_line(fd1);
+	if (!(tab = (char **)malloc(sizeof(char *) * (nb + 1))))
+		return (NULL);
+	while (get_next_line(fd, &line) > 0)
+	{
+		tab[i] = ft_strdup(line);
+		free(line);
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
 }
+
+void		ft_fill_data(char *tab, t_sector *sector)
+{
+	if (ft_strstr(tab, "floorHeight"))
+		sector->floor_height =
+		ft_atoi(ft_strrchr(tab, '=') + 1);
+	if (ft_strstr(tab, "ceilHeight"))
+		sector->ceil_height = ft_atoi(ft_strrchr(tab, '=') + 1);
+}
+
+void		ft_data_storing(int fd, int fd1)
+{
+	char		**tab;
+	int			i;
+	t_sector	*sector;
+
+	i = -1;
+	sector = NULL;
+	tab = ft_fill_map(fd, fd1);
+	ft_parse_error(tab);
+	while (tab[++i])
+	{
+		if (ft_strstr(tab[i], "Sector"))
+		{
+			add_sector(&sector, new_sector());
+			while ((ft_strchr(tab[i], '}') == NULL ||
+			ft_strchr(tab[i - 1], '}') == NULL))
+			{
+				ft_fill_data(tab[i], sector);
+				if (ft_strstr(tab[i], "line"))
+				{
+					add_linedef(&sector->lines, new_linedef());
+					ft_fill_coord(sector, tab, i);
+					//printf("floor_heignt = %d\n", sector->floor_height);
+					//printf("ceil_height = %d\n", sector->ceil_height);
+					//printf("p1.x = %d p1.y = %d\n", sector->lines->p1.x, sector->lines->p1.y);
+					//printf("p2.x = %d p2.y = %d\n", sector->lines->p2.x, sector->lines->p2.y);
+					//printf("\n");
+				}
+				i++;
+			}
+		}
+	}
+}
+
