3ced7b4987996f441fafffdc3843c59863712eb1
Alexandre Ducimetiere <aducimet@z4r1p2.le-101.fr>
2019-05-23 21:02:57 +0200

    change

--- srcs/data/linedef.c
+++ srcs/data/linedef.c
@@ -11,7 +11,7 @@ t_linedef	*init_linedef(t_linedef *line)
 	return (line);
 }
 
-t_linedef	*new_linedef(t_line line, char *name, SDL_Texture *texture, Uint32 flags)
+t_linedef	*new_linedef(t_line line, SDL_Texture *texture, Uint32 flags)
 {
 	t_linedef	*newline;
 
