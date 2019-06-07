7a40a045d8dfa4160b1ddcaf09fbab336bb1fdff
Antoine Giordano <agiordan@z4r1p5.le-101.fr>
2019-06-07 18:41:04 +0200

    Optimisation draw_line

--- srcs/display/simple_draw.c
+++ srcs/display/simple_draw.c
@@ -2,27 +2,25 @@
 
 void	draw_line(t_win *win, t_dot p1, t_dot p2)
 {
-	int             i;
-	int				n_pixels;
-	t_fdot			delta;
-	double			tmp;
-	t_fdot			pixel;
+	int		i;
+	int		n_pixels;
+	t_fdot	delta;
+	t_fdot	absdelta;
+	t_fdot	pixel;
 
 	delta.x = p2.x - p1.x;
 	delta.y = p2.y - p1.y;
-	if (ft_abs(delta.x) > ft_abs(delta.y))
-			tmp = delta.x;
-	else
-			tmp = delta.y;
-	n_pixels = ft_abs(tmp);
-	delta.x /= ft_abs(tmp);
-	delta.y /= ft_abs(tmp);
+	absdelta = (t_fdot){ft_abs(delta.x), ft_abs(delta.y)};
+	n_pixels = absdelta.x > absdelta.y ? absdelta.x : absdelta.y;
+	delta.x /= n_pixels;
+	delta.y /= n_pixels;
 	i = -1;
-	while (i++ < n_pixels)
+	while (++i < n_pixels)
 	{
-		pixel = (t_fdot){.x = p1.x + delta.x * i, .y = p1.y + delta.y * i};
-		if (pixel.y < win->h && pixel.y >= 0 && pixel.x < win->w && pixel.x >= 0)
-				SDL_RenderDrawPoint(win->rend, pixel.x, pixel.y);
+		pixel = (t_fdot){	.x = p1.x + delta.x * i,\
+							.y = p1.y + delta.y * i};
+		if (0 <= pixel.y && pixel.y < win->h && 0 <= pixel.x && pixel.x < win->w)
+			SDL_RenderDrawPoint(win->rend, pixel.x, pixel.y);
 	}
 }
 
