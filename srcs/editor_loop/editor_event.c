be83a763ffdb7642a21ee045fb15bd02dcffb27e
Gael Monachon <gmonacho@z1r4p2.le-101.fr>
2020-01-29 13:33:05 +0100

    selection poly clean

--- srcs/editor_loop/editor_event.c
+++ srcs/editor_loop/editor_event.c
@@ -309,9 +309,6 @@ static void	ed_selection(t_win *win, t_map *map)
 									+ map->editor.pos.y - map->editor.select_rect.y;
 		map->editor.selected_poly = ed_get_selected_poly(map);
 	}
-	// else if (win->winui->mouse.releasing & UI_MOUSE_LEFT)
-	// {
-	// }
 }
 
 static void	ed_action(t_win *win, t_map *map)
