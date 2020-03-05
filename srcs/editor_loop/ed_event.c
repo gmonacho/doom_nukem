6296b48cd5ce64d47d60264e46abeea4e2908352
Gael Monachon <gmonacho@z4r3p4.le-101.fr>
2020-03-05 16:22:23 +0100

    button velocity

--- srcs/editor_loop/ed_event.c
+++ srcs/editor_loop/ed_event.c
@@ -528,7 +528,20 @@ static void	ed_set_buttons_player(t_win *win, t_player *selected)
 		ft_strcpy(text_entry->text, tmp);
 		ft_strdel(&tmp);
 	}
-	ed_clean_property(win, 3);
+	ui_set_text_entry_function(win->winui, "b_property_3", &set_float_value, &selected->const_vel);
+	text_entry = ui_get_text_entry_button(win->winui, "b_property_3");
+	if (text_entry)
+	{
+		if (text_entry->name)
+		{
+			ft_strdel(&text_entry->name);
+			text_entry->name = ft_strdup("velocity");
+		}
+		tmp = ft_itoa(selected->const_vel);
+		ft_strcpy(text_entry->text, tmp);
+		ft_strdel(&tmp);
+	}
+	ed_clean_property(win, 4);
 }
 
 static void	ed_selection(t_win *win, t_map *map)
