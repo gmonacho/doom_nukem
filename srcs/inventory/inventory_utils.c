e33b540c8bd239107c3ef92a95e809bd9b0a8118
Alexandre Ducimetiere <aducimet@z4r4p5.le-101.fr>
2020-03-09 16:40:37 +0100

    norme fichier inventory

--- srcs/inventory/inventory_utils.c
+++ srcs/inventory/inventory_utils.c
@@ -2,5 +2,6 @@
 
 void		print_items_s(t_win *win, SDL_Texture *tex, int slotposx)
 {
-	SDL_RenderCopy(win->rend, tex, NULL, &(SDL_Rect){(slotposx),(win->h * 0.8875), (win->w * 0.075), (win->h * 0.09375)});
+	SDL_RenderCopy(win->rend, tex, NULL, &(SDL_Rect){(slotposx),
+		(win->h * 0.8875), (win->w * 0.075), (win->h * 0.09375)});
 }
