6a4ca154dade86d0bad77ffe08b389ab7a132b34
gmonacho <gaelmonach123@live.fr>
2020-05-10 18:58:41 +0200

    norm srcs

--- srcs/player/player.c
+++ srcs/player/player.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agiordan <agiordan@student.le-101.fr>      +#+  +:+       +#+        */
+/*   By: gal <gal@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 21:15:30 by agiordan          #+#    #+#             */
-/*   Updated: 2020/03/07 21:15:38 by agiordan         ###   ########lyon.fr   */
+/*   Updated: 2020/05/10 18:49:51 by gal              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom_nukem.h"
 
-void    apply_damage(t_map *map, t_player *player, int damage)
+void		apply_damage(t_map *map, t_player *player, int damage)
 {
 	Mix_PlayChannel(4, map->music->tmusic[5], 0);
 	player->currentArmor -= damage;
@@ -23,36 +23,24 @@ void    apply_damage(t_map *map, t_player *player, int damage)
 	}
 }
 
-void    apply_heal(t_player *player, int heal)
-{   
+void		apply_heal(t_player *player, int heal)
+{
 	player->currentHp += heal;
 	if (player->currentHp > 100)
 		player->currentHp = 100;
 }
 
-void    apply_armor(t_player *player, int armor)
-{   
+void		apply_armor(t_player *player, int armor)
+{
 	player->currentArmor += armor;
 	if (player->currentArmor > 100)
 		player->currentArmor = 100;
 }
 
-void    use_item(t_map *map, t_player *player,\
-                    t_music *music, int slot_selected)
+static void	use_item_cut(t_map *map, t_player *player,
+		t_music *music, int slot_selected)
 {
-	if (slot_selected == 0 && player->inventory->item[0]->nb > 0)
-	{
-		player->inventory->item[0]->nb -= 1;
-		Mix_PlayChannel(4, music->tmusic[2], 0);
-		apply_heal(player, 15);
-	}
-	else if (slot_selected == 1 && player->inventory->item[1]->nb > 0)
-	{
-		player->inventory->item[1]->nb -= 1;
-		Mix_PlayChannel(5, music->tmusic[3], 0);
-		apply_armor(player, 15);
-	}
-	else if (slot_selected == 2 && player->inventory->item[2]->nb > 0)
+	if (slot_selected == 2 && player->inventory->item[2]->nb > 0)
 		player->inventory->item[2]->nb -= 1;
 	else if (slot_selected == 3 && player->inventory->item[3]->nb > 0)
 	{
@@ -66,5 +54,23 @@ void    use_item(t_map *map, t_player *player,\
 	}
 	else
 		player->interaction_inventaire = 1;
+}
 
+void		use_item(t_map *map, t_player *player,
+			t_music *music, int slot_selected)
+{
+	if (slot_selected == 0 && player->inventory->item[0]->nb > 0)
+	{
+		player->inventory->item[0]->nb -= 1;
+		Mix_PlayChannel(4, music->tmusic[2], 0);
+		apply_heal(player, 15);
+	}
+	else if (slot_selected == 1 && player->inventory->item[1]->nb > 0)
+	{
+		player->inventory->item[1]->nb -= 1;
+		Mix_PlayChannel(5, music->tmusic[3], 0);
+		apply_armor(player, 15);
+	}
+	else
+		use_item_cut(map, player, music, slot_selected);
 }
